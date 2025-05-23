2bce95731529001e23326e066f2a0afc094d0c2a
Elisee11 <eliseegahouidi8@gmail.com>
2025-05-23 12:44:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -162,7 +162,11 @@ def logout():
     return redirect(url_for('home'))
 
 # Create database and run the app
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+#if __name__ == '__main__':
+    #with app.app_context():
+        #db.create_all()
+    #app.run(debug=True)
+
+ import os 
+ if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT",5000)))
