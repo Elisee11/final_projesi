778ff81ed06514a1adb459cd230014078bedc49f
Elisee11 <eliseegahouidi8@gmail.com>
2025-05-23 16:36:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'gelistirme_anahtari'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///site.db')
+app.config['SQLALCHEMY_DATABASE_URI'] =  'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 # Flask-Login configuration
